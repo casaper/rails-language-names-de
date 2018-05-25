0a99c2dad9eb7ad22ca88755ad6f6de96ffff397
Kaspar Vollenweider <vok@panter.ch>
2018-05-25 11:24:28 +0200

    initial commit

---
+++ lib/rails/language/names/de.rb
@@ -0,0 +1,11 @@
+require "rails/language/names/de/version"
+
+module Rails
+  module Language
+    module Names
+      module De
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "rails/language/names/de"
+
+require "minitest/autorun"
