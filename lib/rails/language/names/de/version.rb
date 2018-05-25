a71424bd68e4baac74aa9f2ec7da7c34473d0b09
Kaspar Vollenweider <vok@panter.ch>
2018-05-25 11:24:28 +0200

    initial commit

---
+++ lib/rails/language/names/de/version.rb
@@ -0,0 +1,9 @@
+module Rails
+  module Language
+    module Names
+      module De
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ test/rails/language/names/de_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Rails::Language::Names::DeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Rails::Language::Names::De::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
